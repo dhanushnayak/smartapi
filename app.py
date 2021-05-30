02b9fbc1766bc7a5c6abe20ff68df7bc3b9af008
Dhanush Nayak <40546930+dhanushnayak@users.noreply.github.com>
2021-05-30 06:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import flask
 import json
 from flask import request, jsonify,make_response
 import MongoData 
+from sms import Sms
 import pandas as pd
 valid = False
 
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
-mongo = MongoData.mongodata()
 
+mongo = MongoData.mongodata()
+i=0
 @app.route('/')
 def home():
     return '''<h1>Smart Helmet Backend</h1>
@@ -17,63 +19,103 @@ def home():
 @app.route("/api/valid/<username>/<password>/",methods=['GET'])
 def user(username,password):
     data = mongo.userdata(username)
+    print(data)
     if data['password']==password:
+        valid=data
         data.pop("_id")
         return jsonify(data)
     else:
         valid=False
         return make_response(jsonify({'valid':False}),404)
 
-@app.route("/api/user/<username>/status/<device>/<status>",methods=['GET'])
+@app.route("/api/user/<username>/status/<status>",methods=['GET'])
 @app.route("/api/user/<username>/status/",methods=['GET'])
-def onthedevice(username,device=None,status=None):
+def onthedevice(username,status=None):
     if status is not None:
-       
-       
         if status.lower()=='on':
-            mongo.setstatus(username,value=status,device=device.lower())
+            mongo.setstatus(username,status)
             d = mongo.getstatus(username)
-            if d is None:
+            if d is  None:
                 return make_response(jsonify({'valid':False}),404)
             df = pd.DataFrame([d]).drop('_id',axis=1).to_json(orient='records')
             print("Data Frame = ",df)
             return jsonify(json.loads(df)[0])
         else:
-            mongo.setstatus(username,value=status,device=device.lower())
+            mongo.setstatus(username,status)
             d = mongo.getstatus(username)
-            if d is None:
+            if d is  None:
                 return make_response(jsonify({'valid':False}),404)
             df = pd.DataFrame([d]).drop('_id',axis=1).to_json(orient='records')
             print("Data Frame = ",df)
             return jsonify(json.loads(df)[0])
-    else:
-            d = mongo.getstatus(username)
-            if d is None:
-                return make_response(jsonify({'valid':False}),404)
-            df = pd.DataFrame([d]).drop('_id',axis=1).to_json(orient='records')
-            print("Data Frame = ",df)
-            return jsonify(json.loads(df)[0])
-            
-        
-@app.route("/api/user/<username>/getalcohol",methods=['GET'])
-def getalcohol(username):
-    
-        data = mongo.getAlcohol(username)
-        if data:
-            df=pd.DataFrame(data)
-            res=df.drop('_id',axis=1).sort_values(by='Time',ascending=True)
-            return jsonify(json.loads(res.to_json(orient='records')))
-        else:
-            return make_response(jsonify({'valid':False}),404)
-   
-@app.route("/api/user/<username>/getlocation",methods=['GET'])
-def getlocation(username):
-    data = mongo.getlocation(username)
-    if data:
-        return jsonify({"lat":data[0],'lon':data[1]})
     else:
         return make_response(jsonify({'valid':False}),404)
 
+@app.route("/api/user/<username>/falldetect/<status>/<speed>",methods=['GET']) 
+def sendsms(username,status,speed):
+    speed1 = int(speed)
+    msg1=''
+    print(status)
+    if status == 'on':
+        if speed1 >= 50 and speed1 <= 60:
+            msg1 = "falldetected with moderate risk"
+        elif speed1 > 60:
+            msg1 = "falldetected with high risk"
+        elif speed1 < 50:
+            msg1 = "falldetected with low risk"
+        print(speed1, msg1)
+        sms1 = Sms(msg=msg1,user=username)
+        send_status =  sms1.Send()
+        if send_status[0]:
+            if mongo.MessageSaved(msg=msg1,user=username,status=True,time=send_status[1]) != None:
+                return jsonify({'sent':msg1})
+        
+    return make_response(jsonify({"msg":msg1}),404)
+                
+@app.route("/api/user/<username>/update/number/<number>",methods=['GET']) 
+def changemynumber(username,number):
+    if mongo.ChangeMyContact(username,number)=='invalid number':
+        return make_response(jsonify({'valid':False,"number":number}),404)
+    else:
+        return jsonify({"Number":number})
+
+@app.route("/api/user/<username>/add/number/<number>",methods=['GET']) 
+def addnumber(username,number):
+    if mongo.AddparentContact(username,number)=='invalid number':
+        return make_response(jsonify({'valid':False,"number":number}),404)
+    else:
+        return jsonify({"Number":number,"added":True})    
+
+@app.route("/api/user/<username>/delete/number/<number>",methods=['GET']) 
+def deletenumber(username,number):
+    if mongo.DeleteparentContact(username,number)=='invalid number':
+        return make_response(jsonify({'valid':False,"number":number}),404)
+    else:
+        return jsonify({"Number":number,"added":True})    
+
+@app.route("/api/user/<username>/getlocation",methods=['GET'])
+def getlocation(username):
+    user = mongo.getlocation(username)
+    if user!=None:
+        return jsonify({"lat":user[0],"log":user[1]})
+    else:
+        return make_response(jsonify({'valid':False}),404)
+
+@app.route("/api/user/<username>/update/location/<lat>/<log>",methods=['GET'])
+def updatelocation(username,lat,log):
+    if mongo.UpdateLocation(user=username,lat=lat,log=log) != None:
+        return jsonify({"Update":"Done","Lat":lat,"log":log})
+    else:
+        return make_response(jsonify({'valid':False}),404)
+
+               
+@app.route("/api/user/<username>/getalcohol",methods=['GET'])
+def getalcohol(username):
+        data = mongo.getAlcohol(username)
+        data.pop('_id')
+        return jsonify(data)
+
+
 if __name__=="__main__":
-    #app.run(host='0.0.0.0', port=5001)
+    #app.run(host='0.0.0.0',port=5001,debug=True)
     app.run(debug=True)
